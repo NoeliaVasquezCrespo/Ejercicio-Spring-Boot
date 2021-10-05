d0a1b0760c1b96ae487139ac3dd1cc598f5440c1
Noelia Vasquez Crespo <noelia.vasquez@ucb.edu.bo>
2021-10-04 21:18:37 -0400

    Agregando spring boot a Git Hub

---
+++ src/main/java/com/example/sakila/appsakila/AppsakilaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sakila.appsakila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsakilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsakilaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sakila/appsakila/AppsakilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sakila.appsakila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppsakilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
